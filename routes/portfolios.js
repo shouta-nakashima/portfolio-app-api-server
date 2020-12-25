11e9b9aa89ecf7f03be7a2f5f274f6bbe4909bfe
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-25 11:14:50 +0900

    get portfolio by id

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getPortfolio } = require("../controllers/portfolios");
+const { getPortfolio, getPortfolioById } = require("../controllers/portfolios");
 
 router.get("", getPortfolio);
+router.get("/:id", getPortfolioById);
 
 module.exports = router;
