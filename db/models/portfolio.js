5780de1f6c1bbb55817a5ce91d549b134985c3de
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-25 10:57:14 +0900

    insert demo data in mongoDB

--- db/models/portfolio.js
+++ db/models/portfolio.js
@@ -2,8 +2,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const portfolioScema = new Schema({
-	title: { type: String },
-	description: String,
+	title: {
+		type: String,
+		required: true,
+	},
+	company: {
+		type: String,
+		required: true,
+	},
+	website: {
+		type: String,
+		required: true,
+	},
+	location: {
+		type: String,
+		required: true,
+	},
+	jobTitle: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	startDate: {
+		type: Date,
+		required: true,
+	},
+	endDate: {
+		type: Date,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 module.exports = mongoose.model("Portfolio", portfolioScema);
