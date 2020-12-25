fef413c058c94574748d69ff8ee97f5e1aab8950
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-25 09:18:04 +0900

    change folder on db

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 const express = require("express");
+const { connect } = require("./db");
 const app = express();
-const config = require("./config/dev");
 
-const mongoose = require("mongoose");
-mongoose.connect(
-	config.DB_URI,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	},
-	(err) => {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log("connect on DB!");
-		}
-	}
-);
+const runServer = async () => {
+	await connect();
 
-const PORT = parseInt(process.env.PORT, 10) || 3001;
+	const PORT = parseInt(process.env.PORT, 10) || 3001;
 
-app.use("/api/v1/portfolios", require("./routes/portfolios"));
+	app.use("/api/v1/portfolios", require("./routes/portfolios"));
 
-app.listen(PORT, (err) => {
-	if (err) console.error(err);
-	console.log(PORT, "で起動してるよ〜！");
-});
+	app.listen(PORT, (err) => {
+		if (err) console.error(err);
+		console.log(PORT, "で起動してるよ〜！");
+	});
+};
+
+runServer();
